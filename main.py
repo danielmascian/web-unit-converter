7879e5ce7ae45f170b5231263a882d426d82b8b8
Daniel <danielmascian@gmail.com>
2026-07-12 15:49:46 +0300

    feat: add page navigation

--- main.py
+++ main.py
@@ -2,9 +2,21 @@ from flask import Flask,render_template,request,url_for,redirect,session,flash
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/home")
 def home():
     return render_template("index.html")
+
+@app.route("/length")
+def length():
+    return render_template("length.html")
+
+@app.route("/weight")
+def weight():
+    return render_template("weight.html")
+
+@app.route("/temperature")
+def temperature():
+    return render_template("temperature.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
