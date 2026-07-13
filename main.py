e58a1df9505e46218802256a1cceda4987e01d20
Daniel <danielmascian@gmail.com>
2026-07-13 13:49:31 +0300

    feat: replace text inputs with select menus for unit selection

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/length")
+@app.route("/length",methods=["GET","POST"])
 def length():
+    if request.method == "POST":
+        value = request.form["value"]
+        metric_from = request.form["metric-from"]
+        metric_to = request.form["metric-to"]
     return render_template("length.html")
 
 @app.route("/weight")
@@ -17,6 +21,15 @@ def weight():
 @app.route("/temperature")
 def temperature():
     return render_template("temperature.html")
+
+
+def calculate_length(value,metric_from,metric_to):
+    length_units = {
+        "km": 1000,
+        "m" :  1,
+        "cm": 0.01,
+        'mm': 0.001
+    }
 
 if __name__ == "__main__":
     app.run(debug=True)
