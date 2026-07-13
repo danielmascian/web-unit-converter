c2259731b586f580577b5d06b7f2e816eeef93cd
Daniel <danielmascian@gmail.com>
2026-07-13 14:52:35 +0300

    feat:add weight conversion result page

--- main.py
+++ main.py
@@ -21,8 +21,19 @@ def length():
         return render_template("length_result.html", result = result , value = value , metric_from = metric_from , metric_to = metric_to)
     return render_template("length.html")
 
-@app.route("/weight")
+@app.route("/weight",methods=["GET","POST"])
 def weight():
+    result = None
+    value = None
+    metric_from = None
+    metric_to = None
+
+    if request.method == "POST":
+        value = float(request.form["value"])
+        metric_from = request.form["metric_from"]
+        metric_to = request.form["metric_to"]
+        result = calculate_weight(value, metric_from, metric_to)
+        return render_template("weight_result.html", result = result , value = value , metric_from = metric_from , metric_to = metric_to)
     return render_template("weight.html")
 
 @app.route("/temperature")
@@ -40,6 +51,18 @@ def calculate_length(value,metric_from,metric_to):
     meters = value * length_units[metric_from]
     rezult = meters / length_units[metric_to]
     return rezult
+
+def calculate_weight(value,metric_from,metric_to):
+    weight_units = {
+        "kg": 1000,
+        "g" : 1,
+        "mg": 0.01,
+        "t": 1_000_000
+    }
+    gram = value * weight_units[metric_from]
+    rezult = gram / weight_units[metric_to]
+    return rezult
+
 
 if __name__ == "__main__":
     app.run(debug=True)
